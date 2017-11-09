0c0bcf438318e66759c5ba53b2f1363f40394f90
Indra Gunawan <feedsbrain@gmail.com>
2017-11-10 01:40:45 +0800

    Add Skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HeaderComponent, FooterComponent
   ],
   imports: [
     BrowserModule,
